ae745335ffb678aba81fbe1409155d7c0d87e6ba
Dmytro <dlmakseverin@gmail.com>
2026-08-03 21:27:05 +0300

    Add React Query provider and update project dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "modern-normalize/modern-normalize.css";
 import "./index.css";
 import App from "./components/App/App";
 
+const queryClient = new QueryClient();
 const container = document.getElementById("app");
 if (!container) {
   throw new Error("Root element not found");
@@ -11,6 +13,8 @@ if (!container) {
 
 createRoot(container).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 );
